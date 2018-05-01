5b237930b762fc54f67b11fae46af822a3b2c44d
llLinkell <37552405+llLinkell@users.noreply.github.com>
2018-05-01 17:06:19 -0300

    cambio 4
    se completo div, lod, str

--- TPfuncional.hs
+++ TPfuncional.hs
@@ -1,7 +1,6 @@
 -- 3.1
 data Procesador = Procesador {memoria :: [Int], acumuladorA :: Int, acumuladorB :: Int, contador :: Int, failure :: String} deriving (Show)
-
-mem = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+mem = replicate 256 0
 
 xt8088 = Procesador {memoria = mem, acumuladorA = 0, acumuladorB = 0, contador = 0, failure = "No hay error" }
 
@@ -14,25 +13,13 @@ lodv valor procesador = procesador {acumuladorA = valor, contador = contador pro
 swap procesador = procesador {acumuladorA = acumuladorB procesador, acumuladorB = acumuladorA procesador, contador = contador procesador +1}
 add procesador = procesador {acumuladorA = acumuladorA procesador + acumuladorB procesador, acumuladorB = 0, contador = contador procesador +1}
 
--- sumar10y22 = add.(loadv 22).swap.(loadv 10) PORQUE MIERDA NO FUNCIONA :(
+sumar10y22 = add.(lodv 22).swap.(lodv 10)
 
 -- 3.4
--- str adress valor procesador = procesador {memoria = take (adress-1) (memoria procesador) ++ valor ++ drop adress (memoria procesador), contador = contador procesador +1}
--- lod adress procesador = procesador {memoria = take (adress-1) (memoria procesador) ++ ([acumuladorA procesador]) ++ drop adress (memoria procesador), contador = contador procesador +1}
 
+divide (Procesador ememoria eacumuladorA 0 econtador _) = Procesador {memoria = ememoria, acumuladorA = eacumuladorA, acumuladorB = 0, contador = econtador + 1, failure = "DIVISION BY ZERO"}
+divide (Procesador ememoria eacumuladorA eacumuladorB econtador efailure) = Procesador {memoria = ememoria, acumuladorA = round (fromIntegral eacumuladorA / fromIntegral eacumuladorB), acumuladorB = 0, contador = econtador + 1, failure = efailure}
+-- se uso la funcion round, pero se puede usar truncate
+str adress valor procesador = procesador { memoria = take (adress -1) (memoria procesador) ++ [valor] ++ drop  adress (memoria procesador)}
 
-
-
-
-
-
-
---dividir procesador|acumladorB procesador == 0 = procesador {failure = "DIVISION BY ZERO"}
---                |otherwise = procesador {acumuladorA = fromIntegral acumuladorA procesador / fromIntegral acumuladorB procesador}
-
--- str adress valor procesador = procesador { memoria = take adress (memoria procesador) ++ valor ++ drop  adress (memoria procesador)}
--- -- -- -- -- -- --
-sumar10y22 = (add procesador).(lodv 22 procesador).(swap procesador).(lodv 10 procesador)
-str adress valor procesador = procesador {memoria = (take (posicionCorregida adress) memoria procesador)++([valor])++(drop adress memoria procesador), contador = contador procesador +1}
-posicionCorregida posicion = posicion-1
-lod adress procesador = procesador {memoria = (take (posicionCorregida adress) memoria procesador) ++ ([acumuladorA procesador]) ++ (drop adress memoria procesador), contador = contador procesador +1}
+lod adress procesador = procesador {acumuladorA = (!!) (memoria procesador) (adress-1)}
