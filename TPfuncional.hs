f6c3ab7cae3e7093581b4d0ff5933611ee3bb914
llLinkell <37552405+llLinkell@users.noreply.github.com>
2018-05-02 11:28:15 -0300

    Cambio 5

--- TPfuncional.hs
+++ TPfuncional.hs
@@ -1,21 +1,21 @@
--- 3.1
+---------- 3.1 ----------
 data Procesador = Procesador {memoria :: [Int], acumuladorA :: Int, acumuladorB :: Int, contador :: Int, failure :: String} deriving (Show)
 mem = replicate 256 0
 
 xt8088 = Procesador {memoria = mem, acumuladorA = 0, acumuladorB = 0, contador = 0, failure = "No hay error" }
 
--- 3.2
+---------- 3.2 ----------
 nop procesador = procesador {contador = contador procesador +1}
-aumentarTresPosiciones procesador = nop.nop.nop
+aumentarTresPosiciones = nop.nop.nop
 
--- 3.3
+---------- 3.3 ----------
 lodv valor procesador = procesador {acumuladorA = valor, contador = contador procesador +1}
 swap procesador = procesador {acumuladorA = acumuladorB procesador, acumuladorB = acumuladorA procesador, contador = contador procesador +1}
 add procesador = procesador {acumuladorA = acumuladorA procesador + acumuladorB procesador, acumuladorB = 0, contador = contador procesador +1}
 
 sumar10y22 = add.(lodv 22).swap.(lodv 10)
 
--- 3.4
+---------- 3.4 ----------
 
 divide (Procesador ememoria eacumuladorA 0 econtador _) = Procesador {memoria = ememoria, acumuladorA = eacumuladorA, acumuladorB = 0, contador = econtador + 1, failure = "DIVISION BY ZERO"}
 divide (Procesador ememoria eacumuladorA eacumuladorB econtador efailure) = Procesador {memoria = ememoria, acumuladorA = round (fromIntegral eacumuladorA / fromIntegral eacumuladorB), acumuladorB = 0, contador = econtador + 1, failure = efailure}
